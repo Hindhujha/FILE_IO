23054f088654beb0f75c476883d14fd3868da176
unknown <hindhujhasridhar@gmail.com>
2022-01-26 10:40:05 +0530

    FILE IO PROGRAMS

---
+++ File_IO/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME TO FILE IO PROGRAMS");
+        }
+    }
+}
